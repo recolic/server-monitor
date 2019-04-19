61a8e6aaf81da42a5e78ea9ebd9141d18e8bb5b1
Recolic Keghart <root@recolic.net>
2019-04-18 23:40:34 -0700

    remove_job_all

--- gen_ci.py
+++ gen_ci.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-ops = [ 'rproxy', 'drive', 'ss-tw', 'ss-us1', 'ss-us5', 'ss-us6', 'ovpn-tw', 'www', 'mail', 'tm', 'git', 'zhixiang', 'mc', 'push-httpdb-agent', 'ddns-wuhan', 'ddns-us', 'dl', 'all' ]
+ops = [ 'rproxy', 'drive', 'ss-tw', 'ss-us1', 'ss-us5', 'ss-us6', 'ovpn-tw', 'www', 'mail', 'tm', 'git', 'zhixiang', 'mc', 'push-httpdb-agent', 'ddns-wuhan', 'ddns-us', 'dl' ]
 
 
 print('''image: recolic/rserver-monitor
