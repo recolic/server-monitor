6aa29da9021f201293893424820f817d7f55cd90
Recolic Keghart <root@recolic.net>
2019-04-18 23:46:11 -0700

    allow_some_failure

--- gen_ci.py
+++ gen_ci.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 ops = [ 'rproxy', 'drive', 'ss-tw', 'ss-us1', 'ss-us5', 'ss-us6', 'ovpn-tw', 'www', 'mail', 'tm', 'git', 'zhixiang', 'mc', 'push-httpdb-agent', 'ddns-wuhan', 'ddns-us', 'dl' ]
+allow_fail = [ 'mc', 'push-httpdb-agent' ]
 
 
 print('''image: recolic/rserver-monitor
@@ -9,5 +10,8 @@ stages:
   - test
 ''')
 for op in ops:
-    print('{}:\n  stage: test\n  script: "/do.bash {}"\n'.format(op, op))
+    print('{}:\n  stage: test\n  script: "/do.bash {}"'.format(op, op))
+    if op in allow_fail:
+        print('  allow_failure: true')
+    print()
 
