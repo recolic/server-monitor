c3ec1296840f749006ea3acf7167a95da28db485
Bensong Liu <bensl@microsoft.com>
2020-09-30 19:00:54 +0800

    Adjust descriptions

--- datafile_gen_daemon_conf.py
+++ datafile_gen_daemon_conf.py
@@ -9,7 +9,7 @@ tests = [
         ('./do.bash ss-us12', 'ShadowSocks US-12'), 
         ('./do.bash ss-hk2', 'ShadowSocks HK-2'), 
         ('./do.bash ovpn-tw', 'OpenVPN taiwan'), 
-        ('./do.bash www', 'Main Website'), 
+        ('./do.bash www', 'Main Website & Blog'), 
         ('./do.bash mail', 'Mail Server'), 
         ('./do.bash tm', 'Teachermate Web Seller'), 
         ('./do.bash git', 'Git'), 
@@ -19,7 +19,7 @@ tests = [
         ('./do.bash dl', 'Download Site'), 
         ('./do.bash shortlink', 'Short Link'), 
         ('./do.bash cc-dns', 'recolic.cc DNS'), 
-        ('./do.bash home-http', 'Home NAS with HTTP'), 
+        ('./do.bash home-http', 'Home NAS & DDNS & HTTP'), 
         ]
 # ('./do.bash ddns-home', 'DDNS home'), 
 
